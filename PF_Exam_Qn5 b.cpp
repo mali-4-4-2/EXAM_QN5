2ce5ee590c01a7807b79fadabd75cdbe84f121fb
mali-4-4-2 <rohnie7@icloud.com>
2024-09-22 13:43:28 +0300

    Add files via upload

---
+++ PF_Exam_Qn5 b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Multiples of 3 from 1 up to 20:" << std::endl;
+    int i = 1;
+
+    // While loop to print multiples of 3 from 1 to 20
+    while (i <= 20) {
+        if (i % 3 == 0) {
+            std::cout << i << std::endl;
+        }
+        ++i; // Increment i
+    }
+
+    return 0;
+}
